bc6ae2252bb9e6dd743d806e694999ff37d6e0fa
Alibek <sultanovalibek2020@gmail.com>
2026-05-30 13:12:51 +0600

    Модель продажи билетов(тут ничо интересного прост слово на англ смешное)

---
+++ src/main/java/kg/attractor/airline/model/Booking.java
@@ -0,0 +1,31 @@
+package kg.attractor.airline.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "bookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticket_id", nullable = false)
+    private Ticket ticket;
+
+    @Column(name = "booked_at", nullable = false)
+    private LocalDateTime bookedAt;
+}
