b8d73d4f4acaf110842ddcf196f636692b4817b1
Alibek <sultanovalibek2020@gmail.com>
2026-05-30 15:47:40 +0600

    Добавлены DTO с валидацией и все шаблоны FreeMarker

--- src/main/java/kg/attractor/airline/dto/CompanyCreateDto.java
+++ src/main/java/kg/attractor/airline/dto/CompanyCreateDto.java
@@ -1,20 +1,23 @@
 package kg.attractor.airline.dto;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CompanyCreateDto {
 
     @NotBlank(message = "Название компании обязательно")
-    @Size(min = 2, max = 200, message = "Название от 2 до 200 символов")
+    @Size(min = 2, max = 200, message = "Название: от 2 до 200 символов")
     private String fullName;
 
-    @NotBlank(message = "Email обязателен")
     @Email(message = "Некорректный формат email")
+    @NotBlank(message = "Email обязателен")
     private String email;
 
     @NotBlank(message = "Пароль обязателен")
--- src/main/java/kg/attractor/airline/dto/FlightCreateDto.java
+++ src/main/java/kg/attractor/airline/dto/FlightCreateDto.java
@@ -1,20 +1,22 @@
 package kg.attractor.airline.dto;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class FlightCreateDto {
 
     @NotBlank(message = "Город вылета обязателен")
-    @Size(min = 2, max = 100, message = "Город от 2 до 100 символов")
+    @Size(min = 2, max = 100, message = "Город вылета: от 2 до 100 символов")
     private String departureCity;
 
     @NotBlank(message = "Город прилёта обязателен")
-    @Size(min = 2, max = 100, message = "Город от 2 до 100 символов")
+    @Size(min = 2, max = 100, message = "Город прилёта: от 2 до 100 символов")
     private String arrivalCity;
 
     @NotBlank(message = "Дата и время вылета обязательны")
